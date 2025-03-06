b7fd3c09b2e47408bbd63e709556c0152b3f0ce4
levilu800b <levilu800b@gmail.com>
2025-03-06 14:47:24 +0000

    ft_print_numbers

---
+++ Fundaments/Intermediate 001/C00/04_ft_print_numbers/ft_print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_print_numbers(void)
+{
+    char numbers = '0';
+
+    while(numbers <= '9')
+    {
+        ft_putchar(numbers);
+        numbers++;
+    }
+}
+
+int main()
+{
+    ft_print_numbers();
+
+    return 0;
+}
