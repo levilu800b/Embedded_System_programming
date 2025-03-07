4f165848c93103c34ab32347c6745ab1909cacde
levilu800b <levilu800b@gmail.com>
2025-03-07 14:49:01 +0000

    ft_is_negative

---
+++ Fundaments/Intermediate 001/C00/05_ft_is_negative/ft_is_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_is_negative(int n)
+{
+    if (n < 0)
+    {
+        ft_putchar('N');
+    }
+    else
+    {
+        ft_putchar('P'); 
+    }
+}
+
+int main()
+{
+    ft_is_negative(0);
+    return 0;
+}
